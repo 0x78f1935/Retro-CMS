6b3f83946b20c749d8c02b5412c484c99f4a2010
undeƒined <codewars.nl@gmail.com>
2022-12-17 23:16:02 +0100

    CHANGE: Delete old CMS

--- backend/utilities/base/__init__.py
+++
@@ -1,8 +0,0 @@
-# encoding: utf-8
-"""
-Base Classes
-------------
-Contains base classes which are used throughout the application
-"""
-from .model import Model as BaseModel  # noqa: F401
-from .parameters import Parameters as ParameterBaseModel
