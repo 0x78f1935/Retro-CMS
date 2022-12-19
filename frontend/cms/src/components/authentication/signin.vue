8963d3409dcf112641503e52b39b4125f2292705
undeƒined <codewars.nl@gmail.com>
2022-12-19 20:50:56 +0100

    CHANGE: Frontend positioning

--- frontend/cms/src/components/authentication/signin.vue
+++ frontend/cms/src/components/authentication/signin.vue
@@ -1,49 +1,32 @@
 <template>
-    <v-card
-      class="mx-auto mt-10"
-      max-width="344"
-    >
-      <v-card-text>
-        <p class="text-h4 text--primary">
-          Sign In
-        </p>
-      </v-card-text>
+  <v-card class="mx-auto mt-10" max-width="344">
+    <v-card-text>
+      <p class="text-h4 text--primary">
+        Sign In
+      </p>
+    </v-card-text>
 
-      <v-form ref="form" v-model="valid" lazy-validation>
-        <v-text-field
-            v-model="name"
-            :counter="10"
-            :rules="nameRules"
-            label="Username"
-            required
-        ></v-text-field>
+    <v-form ref="form" v-model="valid" lazy-validation>
+      <v-text-field v-model="name" :counter="10" :rules="nameRules" label="Username" required></v-text-field>
 
-        <v-text-field
-            v-model="password"
-            :rules="emailRules"
-            label="Passwprd"
-            type="password"
-            required
-        ></v-text-field>
-  </v-form>
+      <v-text-field v-model="password" :rules="emailRules" label="Password" type="password"
+        required></v-text-field>
+    </v-form>
 
-      <v-card-actions>
-        <v-btn
-          variant="text"
-          color="deep-purple-accent-4"
-        >
-          Connect
-        </v-btn>
-      </v-card-actions>
-    </v-card>
-  </template>
+    <v-card-actions>
+      <v-btn variant="text" color="deep-purple-accent-4">
+        Connect
+      </v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
   
-  <script lang='ts'>
-  import { defineComponent } from 'vue'
-  
-  
-  export default defineComponent({
-    name: 'SignIn',
-  })
-  </script>
+<script lang='ts'>
+import { defineComponent } from 'vue'
+
+
+export default defineComponent({
+  name: 'SignIn',
+})
+</script>
   
--- frontend/cms/src/views/HomeView.vue
+++ frontend/cms/src/views/HomeView.vue
@@ -1,8 +1,28 @@
 <template>
-  <section>
-    <signin />
-    <registerVue/>
-  </section>
+  <v-container class="bg-surface-variant" fill-height>
+    <v-row no-gutters>
+
+      <v-col :cols="cols[0]" v-if="cols[0] !=0 ">
+        <v-sheet class="pa-2 ma-2">
+          .v-col-{{ cols[0] }}
+        </v-sheet>
+      </v-col>
+
+      <v-col :cols="cols[1]" v-if="cols[1] != 0">
+        <v-sheet class="pa-2 ma-2">
+          .v-col-{{ cols[1] }}
+        </v-sheet>
+      </v-col>
+
+      <v-col :cols="cols[2]">
+        <v-sheet class="pa-2 ma-2">
+          .v-col-{{ cols[2] }}
+          <Signin/>
+          <registerVue/>
+        </v-sheet>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script lang="ts">
@@ -20,6 +40,29 @@ export default defineComponent({
     Signin,
     registerVue
   },
+
+  computed: {
+    cols () {
+      const { xs, sm, md, lgAndUp  } = this.$vuetify.display;
+      var res;
+      if( xs ) {
+        res = [0, 0, 12];
+      }
+      else if(sm) 
+      {  // Small screens
+        res = [0, 7, 5];
+      }
+      else if (md)
+      {  // Normal size
+        res = [3, 5, 4];
+      }
+      else if (lgAndUp)
+      {  // Large and up
+        res = [3, 6, 3];
+      }
+      return res;
+    },
+  }
 });
 
     Signin</script>
